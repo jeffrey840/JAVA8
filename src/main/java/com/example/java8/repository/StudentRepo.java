22c0171414b1073736c6fe97bf1b2e2cede99cfb
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-22 18:01:24 -0500

    added model

--- src/main/java/com/example/java8/repository/StudentRepo.java
+++ src/main/java/com/example/java8/repository/StudentRepo.java
@@ -1,4 +1,10 @@
 package com.example.java8.repository;
 
-public interface StudentRepo {
+import com.example.java8.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
 }
