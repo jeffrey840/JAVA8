8e4fdac2fadd5cd57bdec6a6f0f6f24f676be579
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-23 00:28:15 -0500

    data is saved to the database

--- src/main/java/com/example/java8/Service/StudentService.java
+++ src/main/java/com/example/java8/Service/StudentService.java
@@ -1,4 +1,7 @@
 package com.example.java8.Service;
 
+import com.example.java8.model.Student;
+
 public interface StudentService {
+      public Student saveStudent(Student student);
 }
--- src/main/java/com/example/java8/Service/StudentServiceImpl.java
+++ src/main/java/com/example/java8/Service/StudentServiceImpl.java
@@ -1,8 +1,29 @@
 package com.example.java8.Service;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Service;
+import com.example.java8.model.Student;
+import com.example.java8.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 
-public class StudentServiceImpl {
+//public class StudentServiceImpl    {
+//
+//}
 
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+//    @Override
+//    public List<Student> getAllStudents() {
+//        return studentRepository.findAll();
+//    }
 }
