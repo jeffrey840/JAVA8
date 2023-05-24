1ce22b780f89d51d267966459ac39dfbe58d2a66
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-24 01:44:36 -0500

    commiting progress

--- studentfrontend/src/App.js
+++ studentfrontend/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Appbar from "../src/components/Appbar.js";
+import Student from "../src/components/Student.js";
 
 function App() {
   return (
     <div className="App">
       <Appbar/>
+        <Student/>
     </div>
   );
 }
